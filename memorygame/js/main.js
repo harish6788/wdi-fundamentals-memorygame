c7b8d423beb577f015d1f61232e4ae0100d15515
Harish Ramesh <harishramesh@Harishs-MBP.hub>
2020-02-09 20:42:16 +1100

    created game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running");
+let cardOne = "King"
+let cardTwo = "queen"
+let cardThree = "King"
+let cardFour = "queen"
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
