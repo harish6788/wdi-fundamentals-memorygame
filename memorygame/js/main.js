f6d052034d81e75ad4595f07b56419e26a745a6d
Harish Ramesh <harishramesh@Harishs-MBP.hub>
2020-02-09 22:10:00 +1100

    added game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -4,20 +4,29 @@ console.log("up and running");
 
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length < 2){
-	alert("flip one more");
-}
-else{
+
+function chechForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		console.log("You found a match");
 	}else {
 		console.log("sorry try again");
 	};
-};
+}
+
+
+function flipCard(cardId){
+	let cardOne = cards[0];
+let cardTwo = cards[2];
+console.log("user flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length < 2) {
+		alert("click one more");
+	}else{
+		chechForMatch();
+	};
+}
+
+    
+
+
